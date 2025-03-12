c7d322e10a3d8cc431bccbcfe7424868c599e37a
TK <tkim186@g.ucla.edu>
2025-03-12 12:50:14 -0700

    trying a new method

--- emg2qwerty/lightning.py
+++ emg2qwerty/lightning.py
@@ -1412,7 +1412,7 @@ class TDSLSTMCTCwTBPTTModule(pl.LightningModule):
 
         if torch.isnan(loss):
             warnings.warn("Loss contains NaN. Setting loss to zero.")
-            loss.data.fill_(0)
+            loss = torch.Tensor(0.0, requires_grad=True)
         return {"loss": loss, "hiddens": hiddens}
 
     def _epoch_end(self, phase: str) -> None:
